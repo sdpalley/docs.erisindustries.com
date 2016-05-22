4c1900719fa65e9339e397eaacce74ab9c791505
David Braun <David@ErisIndustries.com>
2016-05-22 18:56:44 -0400

    Add HTTP server to services-making tutorial test.

--- tutorials/test/services-making/app.js
+++ tutorials/test/services-making/app.js
@@ -1,55 +1,73 @@
-'use strict';
+'use strict'
 
-var
-  contracts = require('eris-contracts'),
-  address = require('./epm.json').deployStorageK,
-  fs = require('fs'),
-  abi = JSON.parse(fs.readFileSync("./abi/" + address)),
-  account = require('./accounts.json'),
-  chainUrl = "http://simplechain:1337/rpc",
-  manager, contract;
+var contracts = require('eris-contracts')
+var http = require('http')
+var address = require('./epm.json').deployStorageK
+var fs = require('fs')
+var abi = JSON.parse(fs.readFileSync('./abi/' + address, 'utf8'))
+var account = require('./accounts.json')
+var chainUrl
+var manager
+var contract
+var server
+
+chainUrl = 'http://simplechain:1337/rpc'
 
 // Instantiate the contract object manager using the chain URL and the account
 // data.
-manager = contracts.newContractManagerDev(chainUrl, account.simplechain_full_000);
+manager = contracts.newContractManagerDev(chainUrl,
+  account.simplechain_full_000)
 
 // Instantiate the contract object using the ABI and the address.
-contract = manager.newContractFactory(abi).at(address);
+contract = manager.newContractFactory(abi).at(address)
 
-// Every second, display Idi's number and decrease it by one until 'times'
-// reaches 0.
-function iterate(times) {
-  if (times > 0) {
-    setTimeout(function () {
-      // Here we get the current number from the contract.
-      contract.get(function (error, result) {
-        var
-          number;
+// Create an HTTP server.
+server = http.createServer(function (request, response) {
+  var
+    body
 
-        if (error)
-          console.error(error);
-        else {
-          number = parseInt(result['c'][0]);
-          console.log("Idi's number is:\t\t\t" + number);
-          console.log("\tlistening at:\t\t\t" + port);
-
-          // Set the new number in the contract.
-          contract.set(number - 1, function (error) {
-            if (error)
-              console.error(error);
-            else
-              iterate(times - 1);
-          });
+  switch (request.message) {
+    case 'GET':
+      // Get the value from the contract and return it to the HTTP client.
+      contract.get(function (error, value) {
+        if (error) {
+          response.statusCode = 500
+        } else {
+          response.statusCode = 200
+          response.setHeader('Content-Type', 'application/json')
+          response.write(value)
         }
-      });
-    }, 3000);
+
+        response.end()
+      })
+
+      break
+
+    case 'PUT':
+      body = ''
+
+      request.on('data', function (chunk) {
+        body += chunk
+      })
+
+      request.on('end', function () {
+        console.log('Setting ' + body)
+
+        // Set the value in the contract.
+        contract.set(JSON.parse(body), function (error) {
+          response.statusCode = error ? 500 : 200
+          response.end()
+        })
+      })
+
+      break
+
+    default:
+      response.statusCode = 501
+      response.end()
   }
-}
+})
 
-// establish a fake port to listen to....
-var port = process.env.IDI_PORT
-
-// note the number of times, defaulting to 5 to do the get -> set reduction
-var times = parseInt(process.env.TIMES) || 5
-
-iterate(times);
+// Tell the server to listen to incoming requests on the port specified in the
+// environment.
+server.listen(process.env.IDI_PORT)
