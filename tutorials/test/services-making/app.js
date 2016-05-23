870bd33457059b7ab8fa43bf197e678d3c908319
David Braun <David@ErisIndustries.com>
2016-05-23 00:44:02 -0400

    Update Services Making tutorial test app to reflect tutorial changes.

--- tutorials/test/services-making/app.js
+++ tutorials/test/services-making/app.js
@@ -1,44 +1,46 @@
 'use strict'
 
 var contracts = require('eris-contracts')
+var fs = require('fs')
 var http = require('http')
 var address = require('./epm.json').deployStorageK
-var fs = require('fs')
 var abi = JSON.parse(fs.readFileSync('./abi/' + address, 'utf8'))
-var account = require('./accounts.json')
+var accounts = require('./accounts.json')
 var chainUrl
 var manager
 var contract
 var server
 
-chainUrl = 'http://simplechain:1337/rpc'
+chainUrl = 'http://localhost:1337/rpc'
 
 // Instantiate the contract object manager using the chain URL and the account
 // data.
 manager = contracts.newContractManagerDev(chainUrl,
-  account.simplechain_full_000)
+  accounts.simplechain_full_000)
 
 // Instantiate the contract object using the ABI and the address.
 contract = manager.newContractFactory(abi).at(address)
 
 // Create an HTTP server.
 server = http.createServer(function (request, response) {
-  var
-    body
+  var body
+  var value
 
-  switch (request.message) {
+  switch (request.method) {
     case 'GET':
+      console.log("Received request to get Idi's number.")
+
       // Get the value from the contract and return it to the HTTP client.
-      contract.get(function (error, value) {
+      contract.get(function (error, result) {
         if (error) {
           response.statusCode = 500
         } else {
           response.statusCode = 200
           response.setHeader('Content-Type', 'application/json')
-          response.write(value)
+          response.write(JSON.stringify(result['c'][0]))
         }
 
-        response.end()
+        response.end('\n')
       })
 
       break
@@ -51,10 +53,11 @@ server = http.createServer(function (request, response) {
       })
 
       request.on('end', function () {
-        console.log('Setting ' + body)
+        value = JSON.parse(body)
+        console.log("Received request to set Idi's number to " + value + '.')
 
         // Set the value in the contract.
-        contract.set(JSON.parse(body), function (error) {
+        contract.set(value, function (error) {
           response.statusCode = error ? 500 : 200
           response.end()
         })
@@ -70,4 +73,7 @@ server = http.createServer(function (request, response) {
 
 // Tell the server to listen to incoming requests on the port specified in the
 // environment.
-server.listen(process.env.IDI_PORT)
+server.listen(process.env.IDI_PORT, function () {
+  console.log('Listening for HTTP requests on port ' + process.env.IDI_PORT +
+    '.')
+})
