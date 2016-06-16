6dfb0aa1e0cd53af7138d9c99681b5cc558dd820
David Braun <David@ErisIndustries.com>
2016-06-16 13:05:15 -0400

    Make chain-making tutorial test more robust.

--- tutorials/test/chain-making/commands.sh
+++ tutorials/test/chain-making/commands.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+echo "Making a Permissioned Chain (Simple) - Eris v0.11"
+echo "https://docs.erisindustries.com/tutorials/chain-making/"
+echo
+
 set -o errexit
 set -o xtrace
 
 chain_dir=$HOME/.eris/chains/simplechain
+
+# Clean up any previous test artifacts first.
+eris chains rm --force simplechain
+rm -rf $chain_dir
+
 chain_dir_this=$chain_dir/simplechain_full_000
 eris services start keys
 eris chains make --account-types=Root:2,Full:1 simplechain
